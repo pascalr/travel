026121e6c6e618ac6bb238ca7f4bbc6c21bb0353
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-23 10:40:25 -0500

    .

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.locals.locale = 'en'
+app.locals.locale = 'fr'
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,6 +31,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res, next) {
   res.render('index')
+})
+
+app.get('/thailand', function(req, res, next) {
+  res.render('thailand')
 })
 
 // catch 404 and forward to error handler
